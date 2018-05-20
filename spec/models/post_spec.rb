e4406210d70173816d0fd5572ed847a273d9b16a
Louis Aurick <louisaurick@ntymail.com>
2018-05-20 14:37:28 +0200

    tests on Post model + Guard Gem installed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context "comment associaiton"
+	it "should increment comments_count" do
+		post1 = Post.new(name: "salut les gens")
+		post1.comments << Comment.new(content: "Ceci est un commentaire")
+		post1.save
+		expect(Comment.count).to eq(1)
+		expect(post1.comments_count).to eq(1)
+	end
+
+  it "should persist a bloc" do 
+  	Post.create(name: "salut les gens")
+  	expect(Post.count).to eq(1)
+  end
 end
